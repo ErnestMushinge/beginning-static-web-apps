cd23222c7b465813f1b1f9a1bca15fbd17f5a25a
ernest <emushinge@gmail.com>
2026-03-08 11:41:52 -0400

    Added markup

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,5 @@
 using Client;
+using Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,6 +8,9 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<BlogpostSummaryService>();
+
+//For Local Azure Function Connection
 //builder.Services.AddScoped(sp =>
 //new HttpClient
 //{
---
+++ Client/Services/BlogpostSummaryService.cs
@@ -0,0 +1,19 @@
+﻿using Models;
+using System.Net.Http.Json;
+
+namespace Client.Services;
+public class BlogpostSummaryService(HttpClient httpClient)
+{
+    private readonly HttpClient _httpClient = httpClient;
+    public List<Blogpost>? Summaries;
+
+
+    public async Task LoadBlogpostSummaries()
+    {
+        if (Summaries == null)
+        {
+            Summaries =
+            await _httpClient.GetFromJsonAsync<List<Blogpost>>("api/blogposts");
+        }
+    }
+}
