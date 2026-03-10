32373337d3f3e3f0846225b775b751c5020c6146
ernest <emushinge@gmail.com>
2026-03-10 14:33:03 -0400

    Added create, edit and delete functionality

---
+++ Models/EditableBlogpost.cs
@@ -0,0 +1,19 @@
+﻿using Models;
+
+public class EditableBlogpost
+{
+    public Guid? Id { get; set; } = null;
+    public string Title { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
+    public DateTime PublishedDate { get; set; }
+    public string BlogpostMarkdown { get; set; } = string.Empty;
+    public Blogpost ToBlogPost(string[] tags) => new Blogpost
+    (
+    Id.GetValueOrDefault(),
+    Title,
+    Author,
+    PublishedDate,
+    tags,
+    BlogpostMarkdown
+    );
+}
